0f4e6bb68999aeb1fdc8f4a653570179ea34eca1
Wondr <wondr.design@gmail.com>
2025-03-15 02:28:50 +0100

    handleSubmit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Form = () => {
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
-      <form className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for your 😍 trip?</h3>
         <select>
           {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
