3af4b2f5120da65086e5cb33173605f513c03349
Wondr <wondr.design@gmail.com>
2025-03-15 03:31:56 +0100

    Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const newItem = {
+      description: description,
+      quantity: quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+    // if the input field is empty
+    if (!description) return;
+    // Clear input fields
+    setDescription("");
+    setQuantity(1);
   };
   return (
     <>
-      <form className="add-form" onSubmit={handleSubmit}>
+      <form id="form" className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for your 😍 trip?</h3>
-        <select>
+        <select
+          id="quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
           {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-            <option value={num} key={num}>
+            <option value={num} id={num} key={num}>
               {num}
             </option>
           ))}
         </select>
-        <input type="text" placeholder="Add an item" />
+        <input
+          id="description"
+          required
+          type="text"
+          placeholder="Add an item"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button type="submit">Add</button>
       </form>
     </>
